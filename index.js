95ea19391e387d871bffb050b434267a440d43ed
mil <miles.sandlar@gmail.com>
2015-04-05 00:10:55 -0500

    standardizing

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var _ = require('lodash');
 var path = require('path');
 var sys = require('sys')
 var exec = require('child_process').exec;
+var basedir = require('xdg').basedir;
 
 var args = process.argv.slice(2);
-var config_folder_path = path.join(__dirname, 'config');
+//var config_folder_path = path.join(__dirname, 'config');
+var config_folder_path = basedir.configPath('lunch');
 var config = require('./modules/config_loader.js')(config_folder_path);
 
 

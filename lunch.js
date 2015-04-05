272701e92e3b122ef86ac5a23c6240516bb5d2d9
mil <miles.sandlar@gmail.com>
2015-04-04 21:06:13 -0500

    various

--- lunch.js
+++ lunch.js
@@ -1,9 +1,35 @@
 #!/usr/bin/env node
 'use strict'
+var _ = require('lodash');
+var path = require('path');
 
-var config = require('./modules/config_loader.js')(
-  __dirname + '/config'
-);
+var args = process.argv.slice(2);
+var config_folder_path = path.join(__dirname, 'config');
+var config = require('./modules/config_loader.js')(config_folder_path);
 
+
+var appetizers_set_array = [];
+_.each(args, function(arg) {
+  _.each(config.lunch_menu, function(menu) {
+    var lunch_list_entries_keys = _.keys(config.lunch_lists[menu].entries);
+    var arg_key_match = lunch_list_entries_keys.indexOf(arg);
+    if (arg_key_match !== -1) {
+      console.log(config.lunch_lists[menu].handled_by);
+      console.log(menu);
+      console.log(lunch_list_entries_keys[arg_key_match]);
+      //appetizers_set_array.push({
+      //  value: 
+      //});
+      //console.log("matches", );
+      
+    }
+    //console.log(menu);
+  });
+
+});
+
+//_.each(config.lunch_menu, function(menu) {
+//  console.log(menu);
+//});
 // canopy peg
-console.log(config);
+//console.log(config);

3583a027dcaee6dffe739ca88ccde2101788c2c6
mil <miles.sandlar@gmail.com>
2015-04-24 17:14:32 -0500

    load custom config folder with --config-folder='somepath'

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 //var path = require('path');
 var sys = require('sys')
 var exec = require('child_process').exec;
+var argv = require('minimist')(process.argv.slice(2));
 var basedir = require('xdg').basedir;
 
-var args = process.argv.slice(2);
-//var config_folder_path = path.join(__dirname, 'config');
-var config_folder_path = basedir.configPath('lunch');
+
+var config_folder_path = argv['config-folder'] ?
+  argv['config-folder'] : basedir.configPath('lunch');
+
+
 var config = require('./lib/config_loader.js')(config_folder_path);
-if (args.length === 0) { process.exit(); }
-
-var command = require('./lib/create_command.js')(args, config);
-
-// run command
+if (argv._.length === 0) { process.exit(); }
+var command = require('./lib/create_command.js')(argv._, config);
 exec(command, function(error, stdout, stderr) {
   sys.puts(stdout);
 });
